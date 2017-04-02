cd64a39d2e80414f6e3e71281b98cfb9b8791a4c
Guillaume de Moffarts <guillaumedemoff@gmail.com>
2017-04-02 15:02:46 +0200

    Add resistance calculator

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 class TentacleForm(BoxLayout):
-    colors = ["Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White","Gold","Silver"]
+    colors = ["Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White"]
+    resistance_input = ObjectProperty()
+    band1_spr = ObjectProperty()
+    band2_spr = ObjectProperty()
+    multi_spr = ObjectProperty()
+
+    def calculateRes(self):
+        bands = int(str(self.colors.index(self.band1_spr.text))+str(self.colors.index(self.band2_spr.text)))
+        multiplier = 10**(self.colors.index(self.multi_spr.text))
+        self.resistance_input .text= str(bands*multiplier)
 
 class TentacleApp(App):
     pass
